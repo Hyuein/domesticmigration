ef7bc3c79cfad8fd342d9df21d31c2445e995dde
Hyuein <102200716+Hyuein@users.noreply.github.com>
2022-04-19 00:39:45 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,204 @@
+var config = {
+    style: 'mapbox://styles/heyhein/ckzhcpaqv000q15qi19x2ep4d',
+    accessToken: 'pk.eyJ1IjoiaGV5aGVpbiIsImEiOiJja3poYnlxN2Q0MDhnMnVwaHc5aTRzbXY1In0.RL4OkZjIJrfoqxaIZjAlmg',
+    showMarkers: false,
+    theme: 'dark',
+    title: '',
+    subtitle: '',
+    byline: '',
+    footer: '',
+    chapters: [
+        {
+            id: 'chapter1',
+            alignment: 'right',
+            title: '1970',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [129.321,35.927],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'sidoname', opacity: 1 },
+                {layer: 'domestic1970icon', opacity: 1 },
+                {layer: 'domestic1970', opacity: 0.7 },
+                {layer: 'domestic1980copy', opacity: 0 },
+                {layer: 'domestic1980', opacity: 0 },
+                {layer: 'domestic1990copy', opacity: 0 },
+                {layer: 'domestic1990', opacity: 0 },
+                {layer: 'domestic2000copy', opacity: 0 },
+                {layer: 'domestic2000', opacity: 0 },
+                {layer: 'domestic2010copy', opacity: 0 },
+                {layer: 'domestic2010', opacity: 0 },
+                {layer: 'domestic2020copy', opacity: 0 },
+                {layer: 'domestic2020', opacity: 0 },
+                {layer: 'pop2020-6t4knrcopy', opacity: 0.7 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter2',
+            alignment: 'right',
+            title: '1980',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [129.321,35.927],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'sidoname', opacity: 1 },
+                {layer: 'domestic1970icon', opacity: 0 },
+                {layer: 'domestic1970', opacity: 0 },
+                {layer: 'domestic1980copy', opacity: 1 },
+                {layer: 'domestic1980', opacity: 0.7 },
+                {layer: 'domestic1990copy', opacity: 0 },
+                {layer: 'domestic1990', opacity: 0 },
+                {layer: 'domestic2000copy', opacity: 0 },
+                {layer: 'domestic2000', opacity: 0 },
+                {layer: 'domestic2010copy', opacity: 0 },
+                {layer: 'domestic2010', opacity: 0 },
+                {layer: 'domestic2020copy', opacity: 0 },
+                {layer: 'domestic2020', opacity: 0 },
+                {layer: 'pop2020-6t4knrcopy', opacity: 0.7 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter3',
+            alignment: 'right',
+            title: '1990',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [129.321,35.927],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'sidoname', opacity: 1 },
+                {layer: 'domestic1970icon', opacity: 0 },
+                {layer: 'domestic1970', opacity: 0 },
+                {layer: 'domestic1980copy', opacity: 0 },
+                {layer: 'domestic1980', opacity: 0 },
+                {layer: 'domestic1990copy', opacity: 1 },
+                {layer: 'domestic1990', opacity: 0.7 },
+                {layer: 'domestic2000copy', opacity: 0 },
+                {layer: 'domestic2000', opacity: 0 },
+                {layer: 'domestic2010copy', opacity: 0 },
+                {layer: 'domestic2010', opacity: 0 },
+                {layer: 'domestic2020copy', opacity: 0 },
+                {layer: 'domestic2020', opacity: 0 },
+                {layer: 'pop2020-6t4knrcopy', opacity: 0.7 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter4',
+            alignment: 'right',
+            title: '2000',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [129.321,35.927],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'sidoname', opacity: 1 },
+                {layer: 'domestic1970icon', opacity: 0 },
+                {layer: 'domestic1970', opacity: 0 },
+                {layer: 'domestic1980copy', opacity: 0 },
+                {layer: 'domestic1980', opacity: 0 },
+                {layer: 'domestic1990copy', opacity: 0 },
+                {layer: 'domestic1990', opacity: 0 },
+                {layer: 'domestic2000copy', opacity: 1 },
+                {layer: 'domestic2000', opacity: 0.7 },
+                {layer: 'domestic2010copy', opacity: 0 },
+                {layer: 'domestic2010', opacity: 0 },
+                {layer: 'domestic2020copy', opacity: 0 },
+                {layer: 'domestic2020', opacity: 0 },
+                {layer: 'pop2020-6t4knrcopy', opacity: 0.7 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter5',
+            alignment: 'right',
+            title: '2010',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [129.321,35.927],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'sidoname', opacity: 1 },
+                {layer: 'domestic1970icon', opacity: 0 },
+                {layer: 'domestic1970', opacity: 0 },
+                {layer: 'domestic1980copy', opacity: 0 },
+                {layer: 'domestic1980', opacity: 0 },
+                {layer: 'domestic1990copy', opacity: 0 },
+                {layer: 'domestic1990', opacity: 0 },
+                {layer: 'domestic2000copy', opacity: 0 },
+                {layer: 'domestic2000', opacity: 0 },
+                {layer: 'domestic2010copy', opacity: 1 },
+                {layer: 'domestic2010', opacity: 0.7 },
+                {layer: 'domestic2020copy', opacity: 0 },
+                {layer: 'domestic2020', opacity: 0 },
+                {layer: 'pop2020-6t4knrcopy', opacity: 0.7 }
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter6',
+            alignment: 'right',
+            title: '2020',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [129.321,35.927],
+                zoom: 7,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'sidoname', opacity: 1 },
+                {layer: 'domestic1970icon', opacity: 0 },
+                {layer: 'domestic1970', opacity: 0 },
+                {layer: 'domestic1980copy', opacity: 0 },
+                {layer: 'domestic1980', opacity: 0 },
+                {layer: 'domestic1990copy', opacity: 0 },
+                {layer: 'domestic1990', opacity: 0 },
+                {layer: 'domestic2000copy', opacity: 0 },
+                {layer: 'domestic2000', opacity: 0 },
+                {layer: 'domestic2010copy', opacity: 0 },
+                {layer: 'domestic2010', opacity: 0 },
+                {layer: 'domestic2020copy', opacity: 1 },
+                {layer: 'domestic2020', opacity: 0.7 },
+                {layer: 'pop2020-6t4knrcopy', opacity: 0.7 }
+            ],
+            onChapterExit: [
+              
+            ]
+        }
+    ]
+};
